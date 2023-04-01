ff9f53e0c326ef0c638a68c0708fce3da1e14dc5
Anand Dubey(Aadi) <115421478+ananddubey1999@users.noreply.github.com>
2023-04-01 19:28:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import '../styles/App.css';
+import React, { useState, useEffect } from 'react';
 
-const App = () => {
-  const [category, setCategory] = useState("general");
-  const [newsData, setNewsData] = useState([]);
-  const [loading, setLoading] = useState();
+function App() {
+  const [category, setCategory] = useState('general');
+  const [news, setNews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(`https://gnews.io/api/v4/top-headlines?category=${category}&apikey=[API_KEY]&max=10&lang=en`)
+      .then(res => res.json())
+      .then(data => {
+        setNews(data.articles);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err));
+  }, [category]);
+
+  const handleCategoryChange = (event) => {
+    setCategory(event.target.value);
+  }
 
   return (
-    <div id="main">
-      <h1 className='heading'>Top 10 {category} news.</h1>
-      <select value={category}>
-        <option value="general">General</option>
-        <option value="business">Business</option>
-        <option value="sports">Sports</option>
-        <option value="technology">Technology</option>
-        <option value="world">World</option>
-        <option value="entertainment">Entertainment</option>
-        <option value="science">Science</option>
-      </select>
-      <p className='loader'>Loading...</p>
-      <ol>
-        <li key="">
-          <img className='news-img' src="" alt=""/>
-          <section className='new-title-content-author'>
-            <h3 className='news-title'>news title</h3>
-            <section className='new-content-author'>
-              <p className='news-description'>news description</p>
-              <p className='news-source'><strong>Source:</strong> source name</p>
-            </section>
-          </section>
-        </li>
-      </ol>
+    <div>
+      <h1 className="heading">Top 10 {category} news</h1>
+      <div>
+        <label htmlFor="category">Select a category:</label>
+        <select id="category" name="category" value={category} onChange={handleCategoryChange}>
+          <option value="general">General</option>
+          <option value="business">Business</option>
+          <option value="entertainment">Entertainment</option>
+          <option value="health">Health</option>
+          <option value="science">Science</option>
+          <option value="sports">Sports</option>
+          <option value="technology">Technology</option>
+        </select>
+      </div>
+      {isLoading ? (
+        <p className="loader">Loading...</p>
+      ) : (
+        <ol>
+          {news.map((article) => (
+            <li key={article.url}>
+              <img src={article.image} alt={article.title} />
+              <div>
+                <h2>{article.title}</h2>
+                <p>{article.description}</p>
+                <p>Source: {article.source.name}</p>
+              </div>
+            </li>
+          ))}
+        </ol>
+      )}
     </div>
-  )
+  );
 }
-
 
 export default App;
